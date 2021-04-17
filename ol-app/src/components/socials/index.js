6d271ce23ca586ad3a1ca13c00890db90ceab269
LiseyPanda <efalck123@yahoo.com>
2021-04-17 17:17:19 -0400

    Adding my updates of Carousel, Cards, Footer, and Socials Components

---
+++ ol-app/src/components/socials/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./socials.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import { isCompositeComponent } from "react-dom/test-utils";
+
+import ig from "./ig.png";
+
+function Socials(props) {
+  return (
+    <Row className="justify-content-md-center">
+      <Col>
+        <img className="social-icons" src={ig}></img>
+      </Col>
+      <Col>
+        <img className="social-icons" src={ig}></img>
+      </Col>
+      <Col>
+        <img className="social-icons" src={ig}></img>
+      </Col>
+    </Row>
+  );
+}
+
+export default Socials;
